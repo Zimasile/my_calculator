efc65b9f9b2881fa8a5677ec29f8b7736eb76c15
zimasile <jaydenngcukana@gmail.com>
2022-11-26 11:41:44 +0200

    update

--- js/code.js
+++ js/code.js
@@ -1,5 +1,7 @@
+// Display
 let display = document.getElementById('output');
 
+// Button-1
 let firstNumber = document.getElementById('button-1');
 
 firstNumber.addEventListener('click', () => {
@@ -7,6 +9,7 @@ firstNumber.addEventListener('click', () => {
     display.value += numb1;
 })
 
+// Button-2
 let secNumber = document.getElementById('button-2');
 
 secNumber.addEventListener('click', () => {
@@ -14,6 +17,7 @@ secNumber.addEventListener('click', () => {
     display.value += numb2;
 })
 
+// Button-3
 let thirdNumber = document.getElementById('button-3');
 
 thirdNumber.addEventListener('click', () => {
@@ -21,6 +25,7 @@ thirdNumber.addEventListener('click', () => {
     display.value += numb3;
 })
 
+// Button-4
 let fourthNumber = document.getElementById('button-4');
 
 fourthNumber.addEventListener('click', () => {
@@ -28,6 +33,7 @@ fourthNumber.addEventListener('click', () => {
     display.value += numb4;
 })
 
+// button-5
 let fifthNumber = document.getElementById('button-5');
 
 fifthNumber.addEventListener('click', () => {
@@ -35,6 +41,7 @@ fifthNumber.addEventListener('click', () => {
     display.value += numb5;
 })
 
+// Button-6
 let sixthNumber = document.getElementById('button-6');
 
 sixthNumber.addEventListener('click', () => {
@@ -42,6 +49,7 @@ sixthNumber.addEventListener('click', () => {
     display.value += numb6;
 })
 
+// Button-7
 let seventhNumber = document.getElementById('button-7');
 
 seventhNumber.addEventListener('click', () => {
@@ -49,6 +57,7 @@ seventhNumber.addEventListener('click', () => {
     display.value += numb7;
 })
 
+// Button-8
 let eighthNumber = document.getElementById('button-8');
 
 eighthNumber.addEventListener('click', () => {
@@ -56,6 +65,7 @@ eighthNumber.addEventListener('click', () => {
     display.value += numb8;
 })
 
+// Button-9
 let ninthNumber = document.getElementById('button-9');
 
 ninthNumber.addEventListener('click', () => {
@@ -63,6 +73,7 @@ ninthNumber.addEventListener('click', () => {
     display.value += numb9;
 })
 
+// Button-0
 let zeroNumber = document.getElementById('button-0');
 
 zeroNumber.addEventListener('click', () => {
@@ -70,21 +81,25 @@ zeroNumber.addEventListener('click', () => {
     display.value += numb0;
 })
 
+// Button-add
 let addition = document.querySelector('#button-add');
 addition.addEventListener('click', ()=>{
     display.value += addition.innerText;
 })
 
+// Button-subtract
 let subtraction = document.querySelector('#button-subtract');
 subtraction.addEventListener('click', ()=>{
     display.value += subtraction.innerText;
 })
 
+// Button-multiply
 let multiplication = document.querySelector('#button-multiply');
 multiplication.addEventListener('click', ()=>{
     display.value += multiplication.innerText;
 })
 
+// Button-divide
 let division = document.querySelector('#button-divide');
 division.addEventListener('click', ()=>{
     display.value += division.innerText;
@@ -97,25 +112,27 @@ nonNumber.addEventListener('click', () => {
     let sum = display.value;
     let a = sum.length -1 ;
     if (sum[a] === '.') {
-        
+
     }
     else{
         let numbnone = nonNumber.innerText;
         display.value += numbnone;
     }
-    
 })
 
+// Clear-button
 let clear = document.querySelector('#button-clear');
 clear.addEventListener('click', ()=>{
     display.value = "";
 })
 
+// Equals-button
 let equals = document.querySelector('.span-1');
 equals.addEventListener('click', ()=>{
     display.value = eval(display.value);
 })
 
+// Button-clear
 let buttonDelete = document.querySelector('#button-del');
 buttonDelete.addEventListener('click', ()=>{
     display.value = display.value.slice(0, -1) ;
